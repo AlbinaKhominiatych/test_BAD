29a55a6b56fdd784be460cf8024e7833eff03a58
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-07-04 15:51:18 +0300

    наповнення словника char_count

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ def find_first_unique_character(text):
     words = text.split() #розбиваю текст на окремі слова
     unique_chars = [] #список для зберігання першого унікального символу
 
+    for word in words:
+        char_count = {} #словник де буква ключ, значення - кількість
+        for char in word: #цикл для окремих слів, де шукаємо унікальний символ
+            if char not in char_count: #унікальність позначається значенням словника 1
+                char_count[char] = 1
+            else:
+                char_count[char] += 1
+
+
 
 
 
