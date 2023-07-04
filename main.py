ba94f8e4cb0bc3661c4508d03b71daf75eae481b
AlbinaHominyatich <hominyatichalbina@gmail.com>
2023-07-04 15:34:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def find_first_unique_character(text):
+    pass
+
+
+text = '''The Tao gave birth to machine language. Machine language gave birth
+to the assembler.
+The assembler gave birth to the compiler. Now there are ten thousand
+languages.
+Each language has its purpose, however humble. Each language
+expresses the Yin and Yang of software. Each language has its place within
+the Tao.
+But do not program in COBOL if you can avoid it.
+        -- Geoffrey James, "The Tao of Programming"'''
+print(find_first_unique_character(text))
